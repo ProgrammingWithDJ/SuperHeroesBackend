5b4cf536487d3f45a9980dfcb090dc78d1c6b9c8
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2022-10-14 10:09:02 +0530

    Add project files.

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroesBackend.Models;
+
+namespace SuperHeroesBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeros()
+        {
+            return new List<SuperHero> { 
+                
+                new SuperHero {
+                       Name ="Spierman",
+                       FirstName ="Peter",
+                       LastName ="Parker",
+                       Place ="Mumbai"
+                           }
+            
+            };
+        }
+    }
+}
